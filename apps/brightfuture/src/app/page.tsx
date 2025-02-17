cb257661c420da7d2b2b79378b0ce4eb322f3e06
Rob Hilgefort <rjhilgefort@gmail.com>
2025-02-17 16:01:46 -0700

    setup tailwind and daisy

--- apps/brightfuture/src/app/page.tsx
+++ apps/brightfuture/src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Page() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
       <Button>Boop</Button>
     </>
   )
