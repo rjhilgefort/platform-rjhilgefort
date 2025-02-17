4a7ce1a6d1b5cda3bcadfb72d906836187623bf3
Rob Hilgefort <rjhilgefort@gmail.com>
2025-02-17 15:37:00 -0700

    install tailwind

--- apps/brightfuture/.eslintrc.js
+++ apps/brightfuture/.eslintrc.js
@@ -5,4 +5,5 @@ module.exports = {
   parserOptions: {
     project: true,
   },
+  ignorePatterns: ['*.config.js', '*.config.mjs'],
 }
--- apps/brightfuture/src/app/page.tsx
+++ apps/brightfuture/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@rjh/core'
 export default function Page() {
   return (
     <>
-      <h1>Web</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button>Boop</Button>
     </>
   )
