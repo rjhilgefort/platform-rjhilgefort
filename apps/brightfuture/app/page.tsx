6bb844cfcb040063c297090ea3868cc01e495194
Rob Hilgefort <rjhilgefort@gmail.com>
2025-02-17 16:43:26 -0700

    rename web, add daisyui

---
+++ apps/brightfuture/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Bright Future</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+    </main>
+  )
+}
