5957391b24be399c36ffcfac62177c160d06237e
Rob Hilgefort <rjhilgefort@gmail.com>
2025-03-10 21:59:24 -0600

    testing gh actions

--- packages/portainer-stack-redeploy/src/index.ts
+++ packages/portainer-stack-redeploy/src/index.ts
@@ -56,6 +56,10 @@ const Effect_logString =
     return Effect.succeed(x)
   }
 
+/**
+ * @url https://github.com/wirgen/portainer-stack-redeploy-action/tree/v1.1
+ * @url https://app.swaggerhub.com/apis/portainer/portainer-ce/2.27.1#/stacks/StackUpdate
+ */
 export const portainerStackRedeploy = async (params: {
   host: string
   accessToken: string
