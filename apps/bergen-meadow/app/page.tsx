2f45596a889255200f94b909610eb18a32424e74
Rob Hilgefort <rjhilgefort@gmail.com>
2025-04-14 21:13:32 -0600

    bergen meadow deploy poc

--- apps/bergen-meadow/app/page.tsx
+++ apps/bergen-meadow/app/page.tsx
@@ -10,9 +10,6 @@ export default function Home() {
   return (
     <div className="w-full">
       <h1>Hello, Bergen Meadow!</h1>
-      <Card title="Card Title" href="https://example.com">
-        <p>Card Content</p>
-      </Card>
     </div>
   )
 }
